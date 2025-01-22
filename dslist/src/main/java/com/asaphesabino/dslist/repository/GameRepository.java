99f5e369b91d88d7a3f9f36da650293925a2ef8a
Asaphe Sabino <asaphhsabino@gmail.com>
2025-01-22 16:55:33 -0300

    GameMinDTO, GameRepository, GameService, GameController

---
+++ dslist/src/main/java/com/asaphesabino/dslist/repository/GameRepository.java
@@ -0,0 +1,8 @@
+package com.asaphesabino.dslist.repository;
+
+import com.asaphesabino.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
---
+++ dslist/src/main/java/com/asaphesabino/dslist/services/GameService.java
@@ -0,0 +1,25 @@
+package com.asaphesabino.dslist.services;
+
+
+import com.asaphesabino.dslist.dto.GameMinDTO;
+import com.asaphesabino.dslist.entities.Game;
+import com.asaphesabino.dslist.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+       List<Game> result = gameRepository.findAll();
+       return result.stream().map(x -> new GameMinDTO(x)).toList();
+
+    }
+
+
+}
