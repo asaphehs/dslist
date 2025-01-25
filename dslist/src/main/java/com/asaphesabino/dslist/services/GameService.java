53dd0ef460d14eacf35642cf3f5132badfadb259
Asaphe Sabino <asaphhsabino@gmail.com>
2025-01-24 23:52:56 -0300

    Consultas BD

--- dslist/src/main/java/com/asaphesabino/dslist/services/GameService.java
+++ dslist/src/main/java/com/asaphesabino/dslist/services/GameService.java
@@ -4,6 +4,7 @@ package com.asaphesabino.dslist.services;
 import com.asaphesabino.dslist.dto.GameDTO;
 import com.asaphesabino.dslist.dto.GameMinDTO;
 import com.asaphesabino.dslist.entities.Game;
+import com.asaphesabino.dslist.projections.GameMinProjection;
 import com.asaphesabino.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,12 @@ public class GameService {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
 
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByGameList(Long listId) {
+        List<GameMinProjection> games = gameRepository.searchByList(listId);
+        return games.stream().map(GameMinDTO::new).toList();
     }
 
 
